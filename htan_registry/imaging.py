05348666d0e3c066be981d6e76927b4d959780be
Adam Taylor <adam.taylor@sagebase.org>
2022-06-08 11:22:39 +0000

    Refactor to imaging class

--- htan_registry/imaging.py
+++ htan_registry/imaging.py
@@ -11,14 +11,14 @@ logger = logging.getLogger(__name__)
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-class TiffInfo(FileTypeFormat):
+class Imaging(FileTypeFormat):
 
-    _filetype = "tiffinfo"
+    _filetype = "imaging"
 
     _process_kwargs = ["databaseSynId"]
 
     def _validate_filetype(self, filePath):
-        assert os.path.basename(filePath[0]).endswith(".tif{1,2}|.svs")
+        assert os.path.basename(filePath[0]).endswith(["tif","tiff",".svs",".ndpi"])
 
     def _process(self, df):
         df.columns = [df.upper() for col in df.columns]
@@ -58,34 +58,35 @@ class TiffInfo(FileTypeFormat):
         if showinf_exc.returncode:
             total_error.append(f"showinf failed: {showinf_exc.stdout}")
 
-        # Validate the OME-XML with xmlvalid
-        xmlvalid_cmd = os.path.join(here, '../bftools/xmlvalid')
-        xmlvalid_exc = subprocess.run([xmlvalid_cmd, path_or_df], capture_output=True, text=True)
-        # If error, set the set a warning
-        if xmlvalid_exc.returncode:
-            warning.append("xmlvalid failed: {xmlvalid_exc.stdout}")
+        # Validate the OME-XML with xmlvalid if file type is ome.tiff
+        if os.path.basename(filePath[0]).endswith(["ome.tif",".ome.tiff"]):
+            xmlvalid_cmd = os.path.join(here, '../bftools/xmlvalid')
+            xmlvalid_exc = subprocess.run([xmlvalid_cmd, path_or_df], capture_output=True, text=True)
+            # If error, set the set a warning
+            if xmlvalid_exc.returncode:
+                total_error.append("ERROR xmlvalid failed:\n\t{xmlvalid_exc.stdout}\n")
 
         # Get all information on the file from tiffinfo
         tiffinfo_exc = subprocess.run(["tiffinfo", path_or_df], capture_output=True, text=True)
         # If error, set the error
         if tiffinfo_exc.returncode:
-            total_error.append("tiffinfo failed: {tiffinfo_exc.stdout}")
+            total_error.append("ERROR tiffinfo failed:\n\t{tiffinfo_exc.stdout}\n")
         # If dates found, make an error
         finddate = len(re.findall(r'date', tiffinfo_exc.stdout, re.IGNORECASE))
         # If likely redacted dates are found make a warning instead
         redacted_dates = len(re.findall(r'1970:01:01 00:00:00', tiffinfo_exc.stdout))
         if redacted_dates:
-            warning.append(f'{finddate} occurences of DATE in TIFF headers that are potentially redacted to "1970:01:01 00:00:00"')
+            total_error.append(f'WARNING: {finddate} occurences of DATE in TIFF headers that are potentially redacted to "1970:01:01 00:00:00"\n')
         elif finddate:
-            total_error.append(f'{finddate} occurences of DATE in TIFF headers')
+            total_error.append(f'ERROR: {finddate} occurences of DATE in TIFF headers\n')
         # If times found make a warning
         findtime = len(re.findall(r'time', tiffinfo_exc.stdout, re.IGNORECASE))
         if findtime:
-            warning.append(f'{findtime} occurences of TIME in TIFF headers') 
+            total_error.append(f'WARNING: {findtime} occurences of TIME in TIFF headers\n') 
         # If label found make a warning
         findlabel = len(re.findall(r'label \d+x\d+', tiffinfo_exc.stdout, re.IGNORECASE))
         if findlabel:
-            warning.append(f'{findlabel} possible label images') 
+            total_error.append(f'WARNING: {findlabel} possible label images\n') 
 
         if total_error == []:
             total_error = ""
